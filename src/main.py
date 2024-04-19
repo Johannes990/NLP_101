6f1227f80b0cf8828b6535c04b5993847cd625b8
Johannes <jojurg@ttu.ee>
2024-04-19 16:13:44 +0300

    starting distilgpt2

--- src/main.py
+++ src/main.py
@@ -2,12 +2,16 @@
 
 from transformers import pipeline
 
-prompt = "This is a new prompt about nothing at all. void of content."
+prompt = "In this course, we will teach you how to"
+candidate_labels = ['barbie', 'school', 'emptiness', 'lucidity', 'nirvana']
 
-
-classifier = pipeline("zero-shot-classification", model="FacebookAI/roberta-large-mnli")
+classifier = pipeline("text-generation", model="distilgpt2")
 answer = classifier(prompt,
-                    candidate_labels=['barbie', 'school', 'emptiness', 'lucidity', 'nirvana'])
+                    num_return_sequences=2,
+                    truncation=True,
+                    max_length=15)
 
-for i in range(len(answer['labels'])):
-    print(f"label {answer['labels'][i]}: probability {100 * answer['scores'][i]:.3f}%")
+# for i in range(len(answer['labels'])):
+#     print(f"label {answer['labels'][i]}: probability {100 * answer['scores'][i]:.3f}%")
+print(answer)
+
