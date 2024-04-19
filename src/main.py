7bbc5c87ff87fd27e4c6ab4c9da8581bd1993cf0
Johannes <jojurg@ttu.ee>
2024-04-19 16:44:09 +0300

    starting distilgpt2 mask filling

--- src/main.py
+++ src/main.py
@@ -2,16 +2,23 @@
 
 from transformers import pipeline
 
-prompt = "In this course, we will teach you how to"
-candidate_labels = ['barbie', 'school', 'emptiness', 'lucidity', 'nirvana']
+# text generation
+textgen_prompt = "Could you please explain to me what is the meaning of"
 
 classifier = pipeline("text-generation", model="distilgpt2")
-answer = classifier(prompt,
-                    num_return_sequences=2,
+answer = classifier(textgen_prompt,
+                    num_return_sequences=5,
                     truncation=True,
-                    max_length=15)
+                    max_length=50)
 
-# for i in range(len(answer['labels'])):
-#     print(f"label {answer['labels'][i]}: probability {100 * answer['scores'][i]:.3f}%")
-print(answer)
+for dicty in answer:
+    for label, gen in dicty.items():
+        print(f"{label}: {gen}")
 
+# mask filling
+mask_prompt = "Who the fuck killed my <mask>."
+unmasker = pipeline("fill-mask")
+
+unmasked = unmasker(mask_prompt, top_k=2)
+for el in unmasked:
+    print(el['sequence'])
